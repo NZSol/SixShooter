cf95dc817a9e31c9975c6e93ffc3acab5b40e559
Fletcher <fpoole666@gmail.com>
2020-07-30 10:56:35 +1200

    Bridge Time

---
+++ Assets/1_Scripts/Environment/BridgeExtender.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeExtender : MonoBehaviour
+{
+
+    public Animator extendBridge;
+    private bool extended; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        extended = false;
+        extendBridge.SetBool("Idle", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
