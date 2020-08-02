c410a4fc2dfb7e669172e03ce37afb0c8b342ed8
Rob <robert.a.nickel@gmail.com>
2020-08-03 11:58:06 +1200

    Testing Getting animstate for script

---
+++ Assets/1_Scripts/AI/AIAttack.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIAttack : MonoBehaviour
+{
+    attackingScript attack;
+    GameObject Player;
+    public bool ExitState;
+    float Dist;
+    Animator anim;
+    [SerializeField] attackingScript attackState;
+    AIBase MasterAI;
+    [SerializeField] float dmgRange;
+
+    private void Start()
+    {
+        Player = GameObject.FindWithTag("Player");
+        anim = GetComponent<Animator>();
+        MasterAI = GetComponent<AIBase>();
+    }
+
+    AnimatorStateInfo currentState;
+    AnimatorStateInfo desiredState;
+    AnimationState animState;
+    private void Update()
+    {
+        if (MasterAI.animCtrl.GetCurrentAnimatorStateInfo(0).IsName("Attacking"))
+        {
+            currentState = MasterAI.animCtrl.GetCurrentAnimatorStateInfo(0);
+        }
+        if (attackState == null)
+        {
+            //animState = MasterAI.animCtrl.GetCurrentAnimatorStateInfo(0).IsName("Attacking");
+        }
+        //Dist = attackState.distance;
+        print(currentState.shortNameHash);
+    }
+
+    public void DoCoroutine()
+    {
+        StartCoroutine(PlayerAttack());
+        print("CheckingCoroutine");
+    }
+
+    public IEnumerator PlayerAttack()
+    {
+        yield return new WaitForSeconds(3);
+        if(Dist < dmgRange)
+        {
+            Player.GetComponent<HealthSystem>().healthReduce(i: 15);
+        }
+        ExitState = true;
+    }
+
+}
--- Assets/1_Scripts/AI/StateMachine/attackingScript.cs
+++ Assets/1_Scripts/AI/StateMachine/attackingScript.cs
@@ -3,26 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+
+
+
 public class attackingScript : StateMachineBehaviour
 {
 
-    GameObject player;
-    float dist;
-    Animator anim;
+    public GameObject player;
+    public Animator anim;
+    public bool attacking;
+    public float distance;
+    bool exitState = false;
+    
+    AIBase scriptMaster;
+    public attackingScript attackScript;
+    AIAttack MonoState;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        attackScript = animator.GetComponent<attackingScript>();
         anim = animator;
         player = GameObject.FindGameObjectWithTag("Player");
-        
+        attacking = true;
+        scriptMaster = animator.GetComponent<AIBase>();
+        MonoState = animator.GetComponent<AIAttack>();
+        MonoState.DoCoroutine();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        dist = Vector3.Distance(player.transform.position, animator.transform.position);
-        //DamagePlayer();
+        exitState = MonoState.ExitState;
+        distance = Vector3.Distance(player.transform.position, anim.transform.position);
+
+        if (exitState == true)
+        {
+            scriptMaster.animCtrl.SetBool("Attacking", false);
+        }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
@@ -42,5 +60,6 @@ public class attackingScript : StateMachineBehaviour
     //{
     //    // Implement code that sets up animation IK (inverse kinematics)
     //}
-    
+
+
 }
