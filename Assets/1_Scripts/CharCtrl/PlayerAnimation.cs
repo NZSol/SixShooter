0e6e5b421c7d981caee0c9a4c55b0e349d601a4c
Rob <robert.a.nickel@gmail.com>
2020-08-04 11:48:07 +1200

    Animations SetUp on press
    Animation Test Scene

--- Assets/1_Scripts/CharCtrl/PlayerAnimation.cs
+++ Assets/1_Scripts/CharCtrl/PlayerAnimation.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class PlayerAnimation : MonoBehaviour
 {
     public Animator playerAnim;
+    [SerializeField] string VertInput;
+    [SerializeField] string HorizInput;
+    [SerializeField] KeyCode shoot;
+    [SerializeField] KeyCode ADS;
+    [SerializeField] KeyCode Reload;
 
     // Start is called before the first frame update
     void Start()
@@ -15,27 +20,44 @@ public class PlayerAnimation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+        if(Input.GetKeyDown(shoot))
         {
             playerAnim.SetBool("Firing", true);
         }
 
-        if (Input.GetButtonUp("Fire1"))
+        if (Input.GetKeyUp(shoot))
         {
             playerAnim.SetBool("Firing", false);
         }
 
-        if (Input.GetAxis("Horizontal") > 0)
+        if (Input.GetAxis(VertInput) > 0 || Input.GetAxis(HorizInput) > 0)
         {
             playerAnim.SetBool("Walking", true);
         }
-        if (Input.GetAxis("Horizontal") < 0)
+        else
         {
             playerAnim.SetBool("Walking", false);
         }
 
+        float vertMove = Input.GetAxis(VertInput);
 
+        if (Input.GetKey(ADS))
+        {
+            playerAnim.SetBool("ADS", true);
+        }
+        else
+        {
+            playerAnim.SetBool("ADS", false);
+        }
 
+        if (Input.GetKey(Reload))
+        {
+            playerAnim.SetBool("Reloading", true);
+        }
+        if (playerAnim.GetBool("Reloading") == true && Input.GetKeyDown(shoot))
+        {
+            playerAnim.SetBool("Reloading", false);
+        }
 
     }
 }
