009885e71fe88748fe8a007bfc9f10c35d3c5344
Unknown <fpoole666@gmail.com>
2020-08-03 19:46:48 +1200

    Flickering Lights/Improved lighting

---
+++ Assets/1_Scripts/Environment/LightFlicker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    public Light thisLight;
+    public float minFlickerSpeed = 0.1f;
+    public float maxFlickerSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(lightFlickering());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+    IEnumerator lightFlickering()
+    {
+        while (true)
+        {
+            thisLight.enabled = true;
+            yield return new WaitForSeconds(Random.Range(minFlickerSpeed, maxFlickerSpeed));
+            thisLight.enabled = false;
+            yield return new WaitForSeconds(Random.Range(minFlickerSpeed, maxFlickerSpeed));
+        }
+
+
+    }
+}
