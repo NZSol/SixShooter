bde2d8678cba24f1d28e0a6469a4c0378948c6a0
Unknown <fpoole666@gmail.com>
2020-08-04 21:25:11 +1200

    Menu-Intro-Game working

---
+++ Assets/1_Scripts/ButtonClick.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class ButtonClick : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip buttonClick;
+    public GameObject optionsPanel;
+    public AudioMixer mixer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        optionsPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void playClip()
+    {
+        audioSource.clip = buttonClick;
+        audioSource.Play();
+    }
+    public void SetLevel (float sliderValue)
+    {
+        mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+    }
+    public void OpenOptions(bool newValue)
+    {
+        optionsPanel.SetActive(newValue);
+
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/1_Scripts/GameStartAfterIntro.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStartAfterIntro : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartGame());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator StartGame()
+    {
+        yield return new WaitForSeconds(25);
+        SceneManager.LoadScene(2);
+    }
+}
