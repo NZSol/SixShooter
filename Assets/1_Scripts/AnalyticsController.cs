075386fc117c4682b034da37d260148ba111d8cd
Josh <joshuaeric.malcolm@gmail.com>
2020-08-10 13:11:44 +1200

    Oil drum plus setting up analytics

---
+++ Assets/1_Scripts/AnalyticsController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnalyticsController : MonoBehaviour
+{
+    /*
+LadderTrigger
+JumpingPuzzleTrigger
+FallingJumpingPuzzleTrigger
+BridgeTrigger
+EnterOfficeTrigger
+ExitOfficeTrigger
+EnterOfficeTrigger
+ExitOfficeTrigger
+EnterCafeteriaTrigger
+ExitCafeteriaTrigger
+EnterPuzzleRoomTrigger
+ExitPuzzleRoomTrigger
+SecretRoomTrigger
+EnterPlatformRoomTrigger
+ExitPlatformRoomTrigger
+EnterComsTrigger
+ExitComsTigger
+EnterLockerTrigger
+EnterEndZoneTrigger
+
+     */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.gameObject.name)
+        {
+            case "LadderTrigger":
+
+            break;
+            case "Ladder":
+
+                break;
+            case "Ladder":
+
+                break;
+            case "Ladder":
+
+                break;
+            case "Ladder":
+
+                break;
+
+        }
+    }
+}
