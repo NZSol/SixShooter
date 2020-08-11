8a3db2c8f9a3c42e5bfae9db176c1fcc9731f3c2
Rob <robert.a.nickel@gmail.com>
2020-08-12 10:00:15 +1200

    Layer Edits. Oilian killable. Ragdoll functioning

--- Assets/1_Scripts/AI/AIBase.cs
+++ Assets/1_Scripts/AI/AIBase.cs
@@ -80,15 +80,22 @@ public class AIBase : MonoBehaviour
         CanSeePlayer();
         HealthCheck();
 
+
+        print(health);
     }
 
     void HealthCheck()
     {
         if (health <= 0)
         {
-            spawnScript.aiCharList.Remove(gameObject);
-            Destroy(GetComponent<Animator>());
+            if(spawnScript)
+            {
+                spawnScript.aiCharList.Remove(gameObject);
+            }
             enableRagdollBones();
+            Destroy(animCtrl);
+            Destroy(GetComponent<NavMeshAgent>());
+            Destroy(this);
         }
     }
 
@@ -141,7 +148,6 @@ public class AIBase : MonoBehaviour
         foreach (Collider col in RagdollParts)
         {
             col.enabled = false;
-            col.isTrigger = true;
         }
         foreach(Collider col in ColliderParts)
         {
