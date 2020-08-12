3482045c3eb38e8cd7cd17b0d9e865aa3f1d3542
Josh <joshuaeric.malcolm@gmail.com>
2020-08-13 11:57:30 +1200

    Added sound trigger not yet hooked up

---
+++ Assets/1_Scripts/Environment/TurnOnSound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnSound : MonoBehaviour
+{
+    public AudioSource[] AI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            for (int i = 0; i < AI.Length; i++)
+            {
+                AI[i].mute = false;
+            }
+        }
+    }
+}
