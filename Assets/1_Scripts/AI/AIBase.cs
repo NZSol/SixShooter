0c3a9eb3f9edd13af25f9f0e5f8b9ec825aee967
Peter <mcdermpete@gmail.com>
2020-08-12 14:52:47 +1200

    Added enemy death sound fx

--- Assets/1_Scripts/AI/AIBase.cs
+++ Assets/1_Scripts/AI/AIBase.cs
@@ -8,6 +8,9 @@ public class AIBase : MonoBehaviour
     GameObject player;
     public Animator animCtrl;
 
+    EnemySoundEvents enemyDeathScript;
+    
+
     //Line of Sight
     float playerDist;
     float minDetectRange = 10;
@@ -66,6 +69,7 @@ public class AIBase : MonoBehaviour
     private void Start()
     {
         player = GameObject.FindWithTag("Player");
+        enemyDeathScript = GetComponent<EnemySoundEvents>();
         animCtrl = GetComponent<Animator>();
         health = 10;
         spawnScript = GetComponentInParent<Spawning>();
@@ -93,6 +97,7 @@ public class AIBase : MonoBehaviour
             }
             enableRagdollBones();
             Destroy(animCtrl);
+            enemyDeathScript.DeathSound();
             Destroy(GetComponent<NavMeshAgent>());
             Destroy(this);
         }
