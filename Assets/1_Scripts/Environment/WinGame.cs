1f828ed438f5263dccc6c8010914ba7daf999351
Rob <robert.a.nickel@gmail.com>
2020-08-13 16:42:04 +1200

    WinCondition code

---
+++ Assets/1_Scripts/Environment/WinGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinGame : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && Interaction.bombRigged)
+        {
+            GameWin();
+        }
+    }
+
+
+    void GameWin()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
